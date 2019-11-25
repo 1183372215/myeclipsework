688b6f4e7ba22f4659f5d3deee512fd946b6a166
lxy <17856927371@163.com>
2019-11-25 18:40:05 +0800

    发布项目

---
+++ xiangmu03/src/com/ahpu/team/domain/Equipment.java
@@ -0,0 +1,7 @@
+package com.ahpu.team.domain;
+
+public interface Equipment {
+	
+	String getDescription();//抽象方法，省略了public abstract
+
+}
---
+++ xiangmu03/src/com/ahpu/team/domain/NoteBook.java
@@ -0,0 +1,11 @@
+package com.ahpu.team.domain;
+
+public class NoteBook implements Equipment{
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
